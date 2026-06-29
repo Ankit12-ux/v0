9e579319e7fe025934169026214d077907752b0d
Ankit Kumar <madheshiya230@gmail.com>
2026-06-30 01:40:11 +0530

    13 message ui

---
+++ src/modules/messages/actions/index.js
@@ -0,0 +1,70 @@
+"use server";
+
+import { MessageRole, MessageType } from "@prisma/client";
+import db from "@/lib/db";
+import { inngest } from "@/inngest/client";
+import { getCurrentUser } from "@/modules/auth/actions";
+
+export const createMessages = async (value, projectId) => {
+  const user = await getCurrentUser();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const project = await db.project.findFirst({
+    where: {
+      id: projectId,
+      userId: user.id,
+    },
+  });
+
+  if (!project) throw new Error("Project not found");
+
+  const newMessage = await db.message.create({
+    data: {
+      projectId,
+      content: value,
+      role: MessageRole.USER,
+      type: MessageType.RESULT,
+    },
+  });
+
+  await inngest.send({
+    name: "code-agent/run",
+    data: {
+      value,
+      projectId,
+    },
+  });
+
+  return newMessage;
+};
+
+
+export const getMessages = async (projectId) => {
+  const user = await getCurrentUser();
+
+  if (!user) throw new Error("Unauthorized");
+
+  const project = await db.project.findFirst({
+    where: {
+      id: projectId,
+      userId: user.id,
+    },
+  });
+
+  if (!project) {
+    throw new Error("Project not found or unauthorized");
+  }
+
+  return await db.message.findMany({
+    where: {
+      projectId,
+    },
+    orderBy: {
+      updatedAt: "asc",
+    },
+    include: {
+      fragments: true,
+    },
+  });
+};
