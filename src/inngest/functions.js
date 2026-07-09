a0f7894f89033f0fd7f3f2ee98a0ab17d955edde
Ankit Kumar <madheshiya230@gmail.com>
2026-07-09 07:27:27 +0530

    17 agent memory

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -20,6 +20,42 @@ export const codeAgentFunction = inngest.createFunction(
       return sandbox.sandboxId;
     });
 
+    const previousMessages = await step.run(
+      "get-previous-messages",
+      async()=>{
+        const formattedMessages = [];
+
+        const messages = await db.message.findMany({
+          where:{
+            projectId:event.data.projectId
+          },
+          orderBy:{
+            createdAt:"desc"
+          }
+        })
+
+        for(const message of messages){
+          formattedMessages.push({
+            type:"text",
+            role:message.role === "ASSISTANT" ? "assistant" : "user",
+            content:message.content
+          })
+        }
+
+        return formattedMessages
+      }
+    )
+
+      const state = createState({
+      summary:"",
+      files:{}
+    }
+  ,
+  {
+    messages:previousMessages
+  }
+)
+
     const codeAgent = createAgent({
       name: "code-agent",
       description: "A simple agent that says hello",
@@ -175,7 +211,7 @@ export const codeAgentFunction = inngest.createFunction(
     });
 
 
-    const result = await network.run(event.data.value);
+    const result = await network.run(event.data.value,{state});
     
     
     const fragmentTitleGenerator = createAgent({
